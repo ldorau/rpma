ac346ae9e1adf0e3166c1f4d35621b3f1c8d90ed
Lukasz Dorau <lukasz.dorau@intel.com>
2021-01-14 11:24:55 +0100

    common: DEBUG

--- utils/docker/run-build.sh
+++ utils/docker/run-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2016-2020, Intel Corporation
+# Copyright 2016-2021, Intel Corporation
 #
 
 #
@@ -173,6 +173,10 @@ cmake .. -DCMAKE_BUILD_TYPE=Debug \
 make -j$(nproc)
 ctest --output-on-failure
 sudo_password -S make -j$(nproc) install
+
+#########################################################
+/bin/bash -i
+#########################################################
 
 if [ "$COVERAGE" == "1" ]; then
 	upload_codecov tests
