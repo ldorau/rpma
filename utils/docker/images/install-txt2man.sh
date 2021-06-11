668aabed54e48be247ff1bc5324bbaa2916f38b5
Lukasz Dorau <lukasz.dorau@intel.com>
2021-06-11 11:15:09 +0200

    test: switch to txt2man-1.7.1

--- utils/docker/images/install-txt2man.sh
+++ utils/docker/images/install-txt2man.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2020, Intel Corporation
+# Copyright 2021, Intel Corporation
 #
 
 #
@@ -13,8 +13,8 @@ set -e
 git clone https://github.com/mvertes/txt2man.git
 cd txt2man
 
-# txt2man v1.7.0
-git checkout txt2man-1.7.0
+# txt2man v1.7.1
+git checkout txt2man-1.7.1
 
 make -j$(nproc)
 sudo make -j$(nproc) install prefix=/usr
